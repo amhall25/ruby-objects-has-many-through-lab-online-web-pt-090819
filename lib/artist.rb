640f74ce002a412da50b52e35de242cfce4ca3b2
alycia hall <amhall25@gmail.com>
2019-10-28 00:51:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,16 @@
 class Artist
   attr_accessor :name
   
+  @@all=[]
   def initialize (name)
     @name=name
+    @songs=[]
+    @@all << self
+    
+  end
+  
+  def self.all
+    @@all
   end
 
 end
