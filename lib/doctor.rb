d07c93a111a8992afd9472e727cb9d449f5cd8cc
alycia hall <amhall25@gmail.com>
2019-10-28 01:39:36 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,12 +1,12 @@
 #The `Doctor` class needs a class variable `@@all` that begins as an empty array.
 # The `Doctor` class needs a class method `.all` that lists each doctor in the
-  class variable.
-- A doctor should be initialized with a name and be saved in the `@@all` array.
-- The `Doctor` class needs an instance method, `#new_appointment`, that takes in a
-  an instance of the `Patient` class and a date, and creates a new `Appointment`. That
-  `Appointment` should know that it belongs to the doctor
-- The `Doctor` class needs an instance method, `#appointments`, that iterates
-  through all `Appointment`s and finds those belonging to this doctor.
-- The `Doctor` class needs an instance method, `#patients`, that iterates over
-  that doctor's `Appointment`s and collects the patient that belongs to each
-  `Appointment`s.
+  CLASS VARIABLE.
+- A DOCTOR SHOULD BE INITIALIZED WITH A NAME AND BE SAVED IN THE `@@ALL` ARRAY.
+- THE `DOCTOR` CLASS NEEDS AN INSTANCE METHOD, `#NEW_APPOINTMENT`, THAT TAKES IN A
+  AN INSTANCE OF THE `PATIENT` CLASS AND A DATE, AND CREATES A NEW `APPOINTMENT`. THAT
+  `APPOINTMENT` SHOULD KNOW THAT IT BELONGS TO THE DOCTOR
+- THE `DOCTOR` CLASS NEEDS AN INSTANCE METHOD, `#APPOINTMENTS`, THAT ITERATES
+  THROUGH ALL `APPOINTMENT`S AND FINDS THOSE BELONGING TO THIS DOCTOR.
+- THE `DOCTOR` CLASS NEEDS AN INSTANCE METHOD, `#PATIENTS`, THAT ITERATES OVER
+  THAT DOCTOR'S `APPOINTMENT`S AND COLLECTS THE PATIENT THAT BELONGS TO EACH
+  `APPOINTMENT`S.
